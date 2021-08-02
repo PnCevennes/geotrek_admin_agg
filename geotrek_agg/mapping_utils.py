fc875224375b9a12267b844c8e04b3865c3b867e
IdrissaD <idrizza@hotmail.fr>
2021-08-02 10:21:00 +0200

    non-prise en compte label ON CONFLICT

--- geotrek_agg/mapping_utils.py
+++ geotrek_agg/mapping_utils.py
@@ -10,14 +10,13 @@ def insert_cor_data(DB, db_source, cor_table, fields):
         Insertion des données dans la table geotrekagg_correspondances
     """
 
-    sql = """
+    sql_del = """
         WITH
             a AS (
             SELECT * FROM public.geotrekagg_correspondances gc
             WHERE NOT EXISTS (
                 SELECT * FROM {db_source}.{cor_table} p
-                WHERE gc.label_origin = p.{label}
-                AND gc.id_origin = p.id
+                WHERE gc.id_origin = p.id
                 )
             AND bdd_source = '{db_source}'
             AND table_origin = '{cor_table}'
@@ -26,14 +25,20 @@ def insert_cor_data(DB, db_source, cor_table, fields):
         USING a
         WHERE gc.bdd_source = a.bdd_source
         AND gc.table_origin = a.table_origin
-        AND gc.label_origin = a.label_origin
         AND gc.id_origin = a.id_origin;
 
+    """
+
+    sql_ins="""
         INSERT INTO public.geotrekagg_correspondances
             (bdd_source, table_origin, id_origin, label_origin)
-        SELECT  '{db_source}', '{cor_table}', id, {label} FROM {db_source}.{cor_table}
-        ON CONFLICT ON CONSTRAINT geotrekagg_correspondances_bdd_source_table_origin_id_origi_key DO NOTHING;
+        SELECT  '{db_source}', '{cor_table}', id, "{label}" FROM {db_source}.{cor_table}
+        ON CONFLICT ON CONSTRAINT geotrekagg_correspondances_bdd_source_table_origin_id_origi_key
+        DO UPDATE
+        SET label_origin = excluded.label_origin;
     """
+
+    sql = sql_del + sql_ins
     try:
         DB.engine.execute(
             text(sql.format(db_source=db_source, cor_table=cor_table, label=fields)).execution_options(autocommit=True)
